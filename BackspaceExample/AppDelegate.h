ef0c4feba234102b5f9438a68bccb9b2c44be8c0
Asif Ahmed <asifahmed2011@gmail.com>
2014-06-20 19:42:04 -0700

    initial commit

---
+++ BackspaceExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BackspaceExample
+//
+//  Created by Asif Ahmed on 6/20/14.
+//  Copyright (c) 2014 Asif Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BackspaceExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BackspaceExample
+//
+//  Created by Asif Ahmed on 6/20/14.
+//  Copyright (c) 2014 Asif Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
